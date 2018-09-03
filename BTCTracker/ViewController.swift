d5260c4abd3671156b2ae8015058c76c3715ba5c
sabg@paul-kang.com <sabg@paul-kang.com>
2018-09-03 22:45:44 +0800

    Final

--- BTCTracker/ViewController.swift
+++ BTCTracker/ViewController.swift
@@ -7,19 +7,91 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+    
 
+    @IBOutlet weak var btcPrice: UILabel!
+    
+    @IBOutlet weak var currencyPicker: UIPickerView!
+    
+    //currency abbreviations https://www.easymarkets.com/int/learn-centre/discover-trading/currency-acronyms-and-abbreviations/
+    
+    let currencyArray = ["USD", "TWD", "CNY", "JPY"]
+    
+    var btcBaseURL = "https://apiv2.bitcoinaverage.com/indices/global/ticker/BTC"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        currencyPicker.delegate   = self
+        currencyPicker.dataSource = self
+        getBTCPrice( url: btcBaseURL + currencyArray[0])
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func getBTCPrice( url: String) {
+        Alamofire.request(url, method: .get).responseJSON {
+            response in
+            if response.result.isSuccess {
+                print("Success!");
+                
+                let btcJSON: JSON = JSON(response.result.value!)
+                
+                let lastResult = btcJSON["last"].double!
+                print(lastResult)
+                self.btcPrice.text = String(lastResult)
+            }
+            else {
+                print("Error \(response.result.error)")
+                self.btcPrice.text = "Error"
+            }
+        }
 
+    }
 
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return currencyArray.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return currencyArray[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        let btcURL = btcBaseURL + String(currencyArray[row])
+        print(btcURL)
+        getBTCPrice( url: btcURL)
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, viewForRow row: Int, forComponent component: Int, reusing view: UIView?) -> UIView {
+        var pickerLabel: UILabel? = (view as? UILabel)
+        if pickerLabel == nil {
+            pickerLabel = UILabel()
+            pickerLabel?.font = UIFont(name: "Arial", size: 40)
+            pickerLabel?.textAlignment = .center
+        }
+        pickerLabel?.text = currencyArray[row]
+        pickerLabel?.textColor = UIColor.white
+
+        return pickerLabel!
+    }
+        
+        
+
+    func pickerView(_ pickerView: UIPickerView, rowHeightForComponent component: Int) -> CGFloat
+    {
+        return 50
+    }
 }
 
